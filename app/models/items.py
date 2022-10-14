e6ea56805ec1f95aba9b4f62a941dee872d4f588
Maliha Omair <maliha@malihaomair.com>
2022-10-14 18:31:36 -0400

    added migration and models

---
+++ app/models/items.py
@@ -0,0 +1,22 @@
+from .db import db
+from flask_login import UserMixin
+
+
+# class Item(db.Model, UserMixin):
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer,db.ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(2000), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+   
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.userId,
+            'title': self.title,
+            'description': self.description,
+            'price': self.price
+        }
