0ac7581aed5a7088620d5713fc4b61142fc338a0
An Bui <dobui0115@gmail.com>
2022-10-14 17:08:56 -0700

    stuck on LoginModal not working

---
+++ react-app/src/components/LoginFormModal/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Modal } from "../../context/Modal";
+import LoginForm from "./LoginForm";
+// import "./LoginForm.css";
+
+function LoginFormModal() {
+  const [showModal, setShowModal] = useState(false);
+console.log("SHOW MODAL NOW",showModal)
+  return (
+    <>
+      <button className="login-button-modal" onClick={() =>{
+         setShowModal(true)
+         console.log("SHOW MODAL NOW",showModal)
+      }} >
+        Log Innnn
+      </button>
+      {showModal && (
+
+        <Modal onClose={() => setShowModal(false)}>
+          <LoginForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default LoginFormModal;
