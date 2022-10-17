340726f56dc1c62060bf16f3c7feed9cf3f90c4a
Standard <oanlara8@gmail.com>
2022-10-17 12:15:49 -0600

    spongebob test for splash circles

--- react-app/src/components/HomePage/LandingPage.js
+++ react-app/src/components/HomePage/LandingPage.js
@@ -1,9 +1,53 @@
 /* TODO:
 if user is not logged in, render <LandingPage /> inside HomePage
 */
-
+import {Link} from 'react-router-dom'
 
 const LandingPage = () => {
+
+
+    const fixed_data = {
+        "Classmate Gifts" : {
+            "imgUrl" : "https://media.discordapp.net/attachments/1017492963720433868/1031627816208445461/spongebob_PNG16.png",
+            "searchUrl" : "/",
+        },
+        "Uncanny Valley" : {
+            "imgUrl" : "https://media.discordapp.net/attachments/1017492963720433868/1031627816208445461/spongebob_PNG16.png",
+            "searchUrl" : "/",
+        },
+        "Holiday" : {
+            "imgUrl" : "https://media.discordapp.net/attachments/1017492963720433868/1031627816208445461/spongebob_PNG16.png",
+            "searchUrl" : "/",
+        },
+        "Staples" : {
+            "imgUrl" : "https://media.discordapp.net/attachments/1017492963720433868/1031627816208445461/spongebob_PNG16.png",
+            "searchUrl" : "/",
+        },
+        "Fancy Living" : {
+            "imgUrl" : "https://media.discordapp.net/attachments/1017492963720433868/1031627816208445461/spongebob_PNG16.png",
+            "searchUrl" : "/",
+        },
+        "Wedding Gifts" : {
+            "imgUrl" : "https://media.discordapp.net/attachments/1017492963720433868/1031627816208445461/spongebob_PNG16.png",
+            "searchUrl" : "/",
+        },
+    }
+
+    const splash_circle_cards = Object.keys(fixed_data).map(name => {
+        return (
+            <div className="splash-circle-card">
+                <Link to="/">
+                        <div className="splash-circle-photo">
+                            <img src={fixed_data[name]["imgUrl"]} />
+                        </div>
+                        <div className="splash-circle-title">
+                            {name}
+                        </div>
+                </Link>
+            </div>
+        )
+    })
+
 
     return (
         <div className="Outer-container">
@@ -13,12 +57,7 @@ const LandingPage = () => {
 
                 </div>
                 <div className="splash-search-circles">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    {splash_circle_cards}
                 </div>
                 <div className="header-bar"></div>
 
