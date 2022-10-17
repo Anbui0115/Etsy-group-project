f5fc631c149b0fa979b7dda075b745728136f9b1
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-17 09:11:53 -0700

    minor changes to fix home page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -44,10 +44,10 @@ function App() {
         <ProtectedRoute path='/users/:userId' exact={true} >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
+        <Route path='/' exact={true} >
           {/* <h1>My Home Page</h1> */}
           <HomePage/>
-        </ProtectedRoute>
+        </Route>
       </Switch>
     </BrowserRouter>
   );
