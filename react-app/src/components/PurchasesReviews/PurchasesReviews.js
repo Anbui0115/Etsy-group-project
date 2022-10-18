35e3f057e896a27d1f18449af2d2f8aa999dec51
Standard <oanlara8@gmail.com>
2022-10-18 13:26:40 -0600

    css for purchasesreview

--- react-app/src/components/PurchasesReviews/PurchasesReviews.js
+++ react-app/src/components/PurchasesReviews/PurchasesReviews.js
@@ -1,36 +1,54 @@
 import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+
 import { useHistory } from "react-router-dom";
 import { getPurchasesAction } from "../../store/session";
-
+import './PurchasesReviews.css'
 /* TODO:
 if user is not logged in, render <LandingPage />
 else render all components for homepage
 */
-const PurchasesReviews=()=>{
+const PurchasesReviews= ()=>{
     const sessionUser = useSelector((state) => state.session.user);
     const history = useHistory();
     const dispatch = useDispatch();
 
-    let n = 1
-    const data = dispatch(getPurchasesAction(n));
+    useEffect(async () => {
+        dispatch(getPurchasesAction(sessionUser.id))
+    }, [dispatch]);
+
+    const purchases = useSelector((state) => state.session.purchases);
+    const itemsObj = useSelector(state => state.items)
+    const items = Object.values(itemsObj)
+
+    let purchaseCards = ( <></>)
+    try {
+        purchaseCards =
+            purchases.map(purchase => {
+                const item = itemsObj[purchase.itemId]
+                return (
+                    <div className="purchase-card">
+                        <div className="purchase-card-image">
+                            <img src={item["images"][0]["image_url"]}></img>
+                        </div>
+                        <div className="purchase-card-right">
+
+                        </div>
+                    </div>
+                )
+            })
+    }
+    catch {
+        purchaseCards = (<></>)
+    }
 
 
-    if(sessionUser){
-       return (
+    if(!sessionUser){ history.push(`/`); }
+    return (
         <div className="purchases-container">
-
+            {purchaseCards}
         </div>
-       )
-    }
-    else{
-        //if the user is not logged in,
-        //the code forces them
-        //to be in the "/login" route
-        //even if I try to go to "/" manually
-        //it would redirect me back to "/login"
-        //need some thinking here
-        history.push(`/`);
-    }
+    )
 
 }
 export default PurchasesReviews
