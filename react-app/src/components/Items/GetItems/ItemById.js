4c0a8f16799171d2b229d4e01d0532aa8facdf2e
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-18 11:16:55 -0700

    added checks for state

--- react-app/src/components/Items/GetItems/ItemById.js
+++ react-app/src/components/Items/GetItems/ItemById.js
@@ -7,6 +7,11 @@ const ItemById = () => {
     const reviewsState = useSelector(state => state.reviews);
     const itemsStateObj = useSelector(state => state.items);
     const items = Object.values(itemsStateObj);
+    const currentUserId = session.user.id;
+
+    if (!itemId) return null;
+    if (!reviewsState) return null;
+    if (!itemsStateObj) return null;
 
     
 
