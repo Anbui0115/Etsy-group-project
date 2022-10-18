85c14c98c112e25d81867833a40b2fbba38e0c98
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-17 21:24:16 -0700

    CSS for footer

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,42 +1,46 @@
+import "./footer.css"
+
 const Footer = () => {
 
     return (
-        <div className="footer-container">
-            <div className="Eatsy-project-repo">
-                <a href="https://github.com/Anbui0115/Etsy-group-project">Eatsy 2022</a>
-            </div>
-            <div className="Dev-info">
-                <div className="dev-name">
-                    Maliha Omair
-                    <div className="dev-links">
-                        <a href="">GitHub</a>
-                        <a href="">LinkedIn</a>
+        <footer>
+            <div className="footer-container">
+                <div className="Eatsy-project-repo">
+                    <a className="Eatsy-link" href="https://github.com/Anbui0115/Etsy-group-project">Eatsy 2022</a>
+                </div>
+                <div className="Dev-info">
+                    <div className="dev-name">
+                        Maliha Omair
+                        <div className="dev-links">
+                            <a className="dev-link" href=""> GitHub </a>
+                            <a className="dev-link" href=""> LinkedIn </a>
+                        </div>
+                    </div>
+                    <div className="dev-name">
+                        Standard Duong
+                        <div className="dev-links">
+                            <a className="dev-link" href="https://github.com/tinsandsolution"> GitHub </a>
+                            <a className="dev-link" href="https://www.linkedin.com/in/laraduong"> LinkedIn </a>
+                        </div>
+                    </div>
+                    <div className="dev-name">
+                        An Bui
+                        <div className="dev-links">
+                            <a className="dev-link" href="https://github.com/Anbui0115"> GitHub </a>
+                            <a className="dev-link" href="https://www.linkedin.com/in/an-bui-0115/"> LinkedIn </a>
+                        </div>
+                    </div>
+                    <div className="dev-name">
+                        Matthew Hutter
+                        <div className="dev-links">
+                            <a className="dev-link" href="https://github.com/Sparky338"> GitHub </a>
+                            <a className="dev-link" href="https://www.linkedin.com/in/matthew-hutter-2a6620173"> LinkedIn </a>
+                        </div>
                     </div>
                 </div>
-                <div className="dev-name">
-                    Standard Duong
-                    <div className="dev-links">
-                        <a href="https://github.com/tinsandsolution">GitHub</a>
-                        <a href="www.linkedin.com/in/laraduong">LinkedIn</a>
-                    </div>
-                </div>
-                <div className="dev-name">
-                    AnBui
-                    <div className="dev-links">
-                        <a href="https://github.com/Anbui0115">GitHub</a>
-                        <a href="https://www.linkedin.com/in/an-bui-0115/">LinkedIn</a>
-                    </div>
-                </div>
-                <div className="dev-name">
-                    Matthew Hutter
-                    <div className="dev-links">
-                        <a href="https://github.com/Sparky338">GitHub</a>
-                        <a href="www.linkedin.com/in/matthew-hutter-2a6620173">LinkedIn</a>
-                    </div>
-                </div>
-            </div>
 
-        </div>
+            </div>
+        </footer>
     )
 };
 
