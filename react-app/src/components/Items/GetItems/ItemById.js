29d79f041da5f57a1be7a86ba230cf0a793779fb
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-18 13:24:38 -0700

    laid out framework and class names

--- react-app/src/components/Items/GetItems/ItemById.js
+++ react-app/src/components/Items/GetItems/ItemById.js
@@ -13,10 +13,28 @@ const ItemById = () => {
     if (!reviewsState) return null;
     if (!itemsStateObj) return null;
 
-    
+
 
     return (
-        <div>
+        <div className="outer-div">
+            <h1> Hi there!</h1>
+            <div className="picture-and-reviews">
+                <div className="pictures">
+                    <div className="picture-list"></div>
+                    <div className="display-picture"></div>
+                </div>
+                <div className="reviews"></div>
+            </div>
+            <div className="item-info-and-cart-button">
+                <div className="store-name">From </div>
+                <div className="item-title-and-price">
+                    <div className="individual-item-title"></div>
+                    <div className="individual-item-price"></div>
+                </div>
+                <div className="add-to-cart-button">{/* render "add to cart"*/ }</div>
+                <div className="item-description"></div>
+            </div>
+
 
         </div>
     )
