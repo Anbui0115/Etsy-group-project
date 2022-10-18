401b7affda327c42cb88133aa2e5afaf302c1315
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-17 20:06:34 -0700

    footer data added

---
+++ react-app/src/components/Footer/Footer.js
@@ -0,0 +1,43 @@
+const Footer = () => {
+
+    return (
+        <div className="footer-container">
+            <div className="Eatsy-project-repo">
+                <a href="https://github.com/Anbui0115/Etsy-group-project">Eatsy 2022</a>
+            </div>
+            <div className="Dev-info">
+                <div className="dev-name">
+                    Maliha Omair
+                    <div className="dev-links">
+                        <a href="">GitHub</a>
+                        <a href="">LinkedIn</a>
+                    </div>
+                </div>
+                <div className="dev-name">
+                    Standard Duong
+                    <div className="dev-links">
+                        <a href="https://github.com/tinsandsolution">GitHub</a>
+                        <a href="www.linkedin.com/in/laraduong">LinkedIn</a>
+                    </div>
+                </div>
+                <div className="dev-name">
+                    AnBui
+                    <div className="dev-links">
+                        <a href="https://github.com/Anbui0115">GitHub</a>
+                        <a href="https://www.linkedin.com/in/an-bui-0115/">LinkedIn</a>
+                    </div>
+                </div>
+                <div className="dev-name">
+                    Matthew Hutter
+                    <div className="dev-links">
+                        <a href="https://github.com/Sparky338">GitHub</a>
+                        <a href="www.linkedin.com/in/matthew-hutter-2a6620173">LinkedIn</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+};
+
+export default Footer
