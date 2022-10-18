f09da79ad89da412507d609a105c8ac91e2ad0b7
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-18 13:55:44 -0700

    rough images layout, shop title filled

--- react-app/src/components/Items/GetItems/ItemById.js
+++ react-app/src/components/Items/GetItems/ItemById.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 const ItemById = () => {
-    const {itemId} = useParams();
+    const { itemId } = useParams();
     const sessionState = useSelector(state => state.session);
     const reviewsState = useSelector(state => state.reviews);
     const itemsStateObj = useSelector(state => state.items);
@@ -13,26 +13,41 @@ const ItemById = () => {
     if (!reviewsState) return null;
     if (!itemsStateObj) return null;
 
+    const filteredItem = items.filter(item => item.id === +itemId)
 
 
     return (
         <div className="outer-div">
-            <div className="picture-and-reviews">
-                <div className="pictures">
-                    <div className="picture-list"></div>
-                    <div className="display-picture"></div>
-                </div>
-                <div className="reviews"></div>
+            <div className="items">
+                {filteredItem.map((item) => {
+                    return (
+                        <div className="item-by-id" key={item.id}>
+                            <div className="picture-and-reviews">
+                                <div className="pictures">
+                                    <div className="picture-list">
+                                        {/* Display carosel here with {item.images} */}
+                                    </div>
+                                    <div className="display-picture">
+                                        <img src={item.images[0].image_url} />
+                                        </div>
+                                </div>
+                                <div className="reviews"></div>
+                            </div>
+                            <div className="item-info-and-cart-button">
+                                <div className="store-name">From {item.owner.username}'s shop</div>
+                                <div className="item-title-and-price">
+                                    <div className="individual-item-title"></div>
+                                    <div className="individual-item-price"></div>
+                                </div>
+                                <div className="add-to-cart-button">{/* render "add to cart"*/}</div>
+                                <div className="item-description"></div>
+                            </div>
+                        </div>
+
+                    )
+                })}
             </div>
-            <div className="item-info-and-cart-button">
-                <div className="store-name">From </div>
-                <div className="item-title-and-price">
-                    <div className="individual-item-title"></div>
-                    <div className="individual-item-price"></div>
-                </div>
-                <div className="add-to-cart-button">{/* render "add to cart"*/ }</div>
-                <div className="item-description"></div>
-            </div>
+
 
 
         </div>
