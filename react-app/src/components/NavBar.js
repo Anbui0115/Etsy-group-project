9d39b2ab7ce0b37e628de178c4f966853abea920
Matthew Hutter <matthewjhutter@gmail.com>
2022-10-20 14:06:42 -0700

    cleaned up code after attempt to add margin to cart

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { NavLink, useHistory } from "react-router-dom";
-import LogoutButton from "./auth/LogoutButton";
 import styles from "./NavBar.module.css";
 import { useSelector } from "react-redux";
 import LoginFormModal from "./LoginFormModal";
@@ -26,24 +25,14 @@ const NavBar = () => {
         <div className="profile_dropdown">
           <ProfileButton />
         </div>
-
-        {/* <div>
-          <LogoutButton />
-        </div> */}
       </>
     );
   } else {
     sessionLinks = (
       <>
-        {/* <NavLink to='/login' exact={true} activeClassName='active'>
-          Login
-        </NavLink> */}
         <div>
           <LoginFormModal />
         </div>
-        {/* <NavLink to='/sign-up' exact={true} activeClassName='active'>
-          Sign Up
-        </NavLink> */}
       </>
     );
   }
@@ -93,9 +82,6 @@ const NavBar = () => {
             </div>
           </div>
           {sessionLinks}
-          {/* <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink> */}
         </nav>
       </div>
     </div>
