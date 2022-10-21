9d170d26ce2bb6b92cac603c874100957a4b3f9f
Standard <oanlara8@gmail.com>
2022-10-20 18:07:38 -0600

    compressed images

--- migrations/versions/migrationfile.py
+++ migrations/versions/migrationfile.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 79d7b88641dc
+Revision ID: c2b8b4a866b9
 Revises: 
-Create Date: 2022-10-19 20:11:45.746140
+Create Date: 2022-10-20 18:05:03.810556
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '79d7b88641dc'
+revision = 'c2b8b4a866b9'
 down_revision = None
 branch_labels = None
 depends_on = None
