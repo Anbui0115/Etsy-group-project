942d4beba93be4bb14cfef8c69270285ba9ecefc
Standard <oanlara8@gmail.com>
2022-10-20 18:15:48 -0600

    faulty seed data

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -7,11 +7,11 @@ def seed_reviews():
     )
 
     reviewName2 = Review(
-        item_id = 1, user_id = 2, purchase_id = 2, stars = 1, title = "I ate it and i died", description = "I will forever haunt whoever sold this. shame on you!"
+        item_id = 1, user_id = 1, purchase_id = 2, stars = 1, title = "I ate it and i died", description = "I will forever haunt whoever sold this. shame on you!"
     )
 
     reviewName3 = Review(
-        item_id = 1, user_id = 3, purchase_id = 3, stars = 5, title = "I ate it and it turned me into a newt", description = "But I got better, so really it's not that bad!"
+        item_id = 1, user_id = 1, purchase_id = 3, stars = 5, title = "I ate it and it turned me into a newt", description = "But I got better, so really it's not that bad!"
     )
 
 
