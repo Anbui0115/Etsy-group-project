65fb17fd3fd08088f1631a34b4253c00cc0e36e3
Matthew Hutter <matthewjhutter@gmail.com>
2023-01-02 08:29:05 -0800

    updated Matt's LinkedIn link, Updated outside link functionality in footer

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -6,35 +6,62 @@ const Footer = () => {
         <footer>
             <div className="footer-container">
                 <div className="Eatsy-project-repo">
-                    <a className="Eatsy-link" href="https://github.com/Anbui0115/Etsy-group-project">Eatsy 2022</a>
+                    <a className="Eatsy-link"
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://github.com/Anbui0115/Etsy-group-project">Eatsy 2022</a>
                 </div>
                 <div className="Dev-info">
                     <div className="dev-name">
                         Maliha Omair
                         <div className="dev-links">
-                            <a className="dev-link" href="https://github.com/maliha-omair"> GitHub </a>
-                            <a className="dev-link" href="https://www.linkedin.com/in/maliha-omair/"> LinkedIn </a>
+                            <a className="dev-link"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/maliha-omair"> GitHub </a>
+                            <a className="dev-link"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://www.linkedin.com/in/maliha-omair/"> LinkedIn </a>
                         </div>
                     </div>
                     <div className="dev-name">
                         Standard Duong
                         <div className="dev-links">
-                            <a className="dev-link" href="https://github.com/tinsandsolution"> GitHub </a>
-                            <a className="dev-link" href="https://www.linkedin.com/in/laraduong"> LinkedIn </a>
+                            <a className="dev-link"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/tinsandsolution"> GitHub </a>
+                            <a className="dev-link"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://www.linkedin.com/in/laraduong"> LinkedIn </a>
                         </div>
                     </div>
                     <div className="dev-name">
                         An Bui
                         <div className="dev-links">
-                            <a className="dev-link" href="https://github.com/Anbui0115"> GitHub </a>
-                            <a className="dev-link" href="https://www.linkedin.com/in/an-bui-0115/"> LinkedIn </a>
+                            <a className="dev-link"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/Anbui0115"> GitHub </a>
+                            <a className="dev-link"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://www.linkedin.com/in/an-bui-0115/"> LinkedIn </a>
                         </div>
                     </div>
                     <div className="dev-name">
                         Matthew Hutter
                         <div className="dev-links">
-                            <a className="dev-link" href="https://github.com/Sparky338"> GitHub </a>
-                            <a className="dev-link" href="https://www.linkedin.com/in/matthew-hutter-2a6620173"> LinkedIn </a>
+                            <a className="dev-link"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/Sparky338"> GitHub </a>
+                            <a className="dev-link"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://www.linkedin.com/in/matt-hutter-2a6620173"> LinkedIn </a>
                         </div>
                     </div>
                 </div>
